cc48e3b6e2d16e292cc7017eef47c8203daf2099
Nick <nick.syndi@gmail.com>
2015-07-21 23:11:07 +0300

    Fix

--- collections/BooksCollection.js
+++ collections/BooksCollection.js
@@ -11,7 +11,7 @@ define([
     model: Book,
 
     url: 'http://10.47.90.173:8080/api/books',
-    // url: 'http://localhost:8080/api/books',
+    //url: 'http://localhost:8080/api/books',
 
     // sort book by title
     comparator: 'title',
@@ -20,14 +20,8 @@ define([
       var self = this;
 
       this.listenTo(this, 'all', function (eventName) {
-        console.info('collection', eventName)
+        console.info('collection', eventName);
       });
-
-      this.listenTo(this, 'sync sort', function () {
-        // require(['view/books'], function (books) {
-        // 	books.renderBooks(self.toJSON());
-        // })
-      })
     }
   });
 });
